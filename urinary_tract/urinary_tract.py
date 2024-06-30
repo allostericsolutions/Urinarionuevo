3006d2f78903366af454d058944395c2e18432f6
allostericsolutions <ciclosporina2@hotmail.com>
2024-06-30 13:02:58 -0400

    Update urinary_tract.py

--- urinary_tract/urinary_tract.py
+++ urinary_tract/urinary_tract.py
@@ -8,9 +8,11 @@ if 'mostrar_eponimos' not in st.session_state:
 def style_expander_title(title):
     return f"<span style='color:gold; font-weight:bold;'>{title}</span>"
 
-# Sección que se expande y se contrae 
-with st.expander(" ", expanded=st.session_state.mostrar_eponimos):
-    st.markdown(style_expander_title("Eponyms related to Urinary Tract Conditions"), unsafe_allow_html=True)
-    st.write("**Henoch-Schönlein purpura:**  Is an inflammatory disorder that affects small blood vessels, causing a purple rash on the skin, abdominal and joint pain, and can also affect the kidneys.")
-    st.write("**Nutcracker syndrome:**  Is a condition caused by compression of the left renal vein between the aorta and the superior mesenteric artery, which can lead to problems with renal blood flow.")
-    st.write("**Wilms tumor (nephroblastoma):**  Is a type of kidney cancer that primarily affects children. It can be referred to as nephroblastoma and can be life-threatening if left untreated.")
+# Crear un título estilizado para el expander
+expander_title = style_expander_title("Eponyms related to Urinary Tract Conditions")
+
+# Sección que se expande y se contrae
+with st.expander(expander_title, expanded=st.session_state.mostrar_eponimos):
+    st.write("**Henoch-Schönlein purpura:** Is an inflammatory disorder that affects small blood vessels, causing a purple rash on the skin, abdominal and joint pain, and can also affect the kidneys.")
+    st.write("**Nutcracker syndrome:** Is a condition caused by compression of the left renal vein between the aorta and the superior mesenteric artery, which can lead to problems with renal blood flow.")
+    st.write("**Wilms tumor (nephroblastoma):** Is a type of kidney cancer that primarily affects children. It can be referred to as nephroblastoma and can be life-threatening if left untreated.")
