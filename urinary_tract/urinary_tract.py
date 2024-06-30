24e27f13faf8bb72e38b4a7ce1e4780020d37721
allostericsolutions <ciclosporina2@hotmail.com>
2024-06-30 13:06:09 -0400

    Update urinary_tract.py

--- urinary_tract/urinary_tract.py
+++ urinary_tract/urinary_tract.py
@@ -4,15 +4,19 @@ import streamlit as st
 if 'mostrar_eponimos' not in st.session_state:
     st.session_state.mostrar_eponimos = False
 
-# Función para estilizar los títulos
-def style_expander_title(title):
-    return f"<span style='color:gold; font-weight:bold;'>{title}</span>"
+# Botón para alternar la visibilidad del expander
+if st.button("Eponyms"):
+    st.session_state.mostrar_eponimos = not st.session_state.mostrar_eponimos
+
+# Función para estilizar los títulos dentro del expander
+def style_title(title):
+    return f"<span style='color:darkorange; font-weight:bold;'>{title}</span>"
 
 # Crear un título estilizado para el expander
-expander_title = style_expander_title("Eponyms related to Urinary Tract Conditions")
+expander_title = "<span style='color:gold; font-weight:bold;'>Eponyms related to Urinary Tract Conditions:</span>"
 
 # Sección que se expande y se contrae
 with st.expander(expander_title, expanded=st.session_state.mostrar_eponimos):
-    st.write("**Henoch-Schönlein purpura:** Is an inflammatory disorder that affects small blood vessels, causing a purple rash on the skin, abdominal and joint pain, and can also affect the kidneys.")
-    st.write("**Nutcracker syndrome:** Is a condition caused by compression of the left renal vein between the aorta and the superior mesenteric artery, which can lead to problems with renal blood flow.")
-    st.write("**Wilms tumor (nephroblastoma):** Is a type of kidney cancer that primarily affects children. It can be referred to as nephroblastoma and can be life-threatening if left untreated.")
+    st.markdown(style_title("Henoch-Schönlein purpura:") + " Is an inflammatory disorder that affects small blood vessels, causing a purple rash on the skin, abdominal and joint pain, and can also affect the kidneys.", unsafe_allow_html=True)
+    st.markdown(style_title("Nutcracker syndrome:") + " Is a condition caused by compression of the left renal vein between the aorta and the superior mesenteric artery, which can lead to problems with renal blood flow.", unsafe_allow_html=True)
+    st.markdown(style_title("Wilms tumor (nephroblastoma):") + " Is a type of kidney cancer that primarily affects children. It can be referred to as nephroblastoma and can be life-threatening if left untreated.", unsafe_allow_html=True)
