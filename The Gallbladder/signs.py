f6fe48c1d05b85ded85067fb1f59a62695429280
allostericsolutions <ciclosporina2@hotmail.com>
2024-07-07 15:56:49 -0400

    Update signs.py

--- The Gallbladder/signs.py
+++ The Gallbladder/signs.py
@@ -1,26 +1,20 @@
 import streamlit as st
 
 # Initialize the variable in session_state
-if 'mostrar_signos' not in st.session_state:  # Changed to 'mostrar_signos'
-    st.session_state.mostrar_signos = False
+if 'mostrar_eponimos' not in st.session_state:
+    st.session_state.mostrar_eponimos = False
 
-# Function to style the sign titles
+# Function to style the eponym titles
 def style_title(title):
     return f"<span style='color:darkorange; font-weight:bold;'>{title}</span>"
 
-# Gallbladder Signs
-gallbladder_signs = [ 
-    ("Ball-on-the-wall sign", "Appearance of a gallbladder polyp."),
-    ("Cinnamon bun sign", "Short axis appearance of intussusception."),
-    ("Whirlpool sign", "Cystic duct appearance with color Doppler associated with gallbladder torsion."),
-    ("Wall-echo-shadow (WES) sign", "Appearance of a gallbladder completely filled with stones."),
-    ("Olive sign", "Palpable hypertrophic pyloric muscle associated with pyloric stenosis"),
-    ("Murphy sign", "Pain with probe pressure over the gallbladder."),
-    ("Pseudogallbladder sign", "Cystic structure noted in the gallbladder fossa without evidence of an actual gallbladder; associated with biliary atresia in children.")
-]
-
-# Expandable and collapsible section for Gallbladder Signs
-with st.expander("Gallbladder Ultrasound Signs:", expanded=st.session_state.mostrar_signos):  # Changed to 'mostrar_signos'
-    for sign, description in gallbladder_signs:
-        st.markdown(style_title(sign) + f": {description}", unsafe_allow_html=True)
-
+# Expandable and collapsible section
+with st.expander("Signs related to Gallbladder:", expanded=st.session_state.mostrar_eponimos):
+    st.markdown(style_title("Ball-on-the-wall sign:") + " Appearance of a gallbladder polyp.", unsafe_allow_html=True)
+    st.markdown(style_title("Cinnamon bun sign:") + " Short axis appearance of intussusception ", unsafe_allow_html=True)
+    st.markdown(style_title("Whirlpool sign:") + " Cystic duct appearance with color Doppler associated with gallbladder torsion.", unsafe_allow_html=True)
+    st.markdown(style_title("Wall-echo-shadow (WES) sign:") + " Appearance of a gallbladder completely filled with stones.", unsafe_allow_html=True)
+    st.markdown(style_title("Olive sign:") + " Palpable hypertrophic pyloric muscle associated with pyloric stenosis", unsafe_allow_html=True)
+    st.markdown(style_title("Murphy sign:") + " Pain with probe pressure over the gallbladder.", unsafe_allow_html=True)
+    st.markdown(style_title("Pseudogallbladder sign:") + " Cystic structure noted in the gallbladder fossa without evidence of an actual gallbladder; associated with biliary atresia in children.", unsafe_allow_html=True)
+I've simply removed the original eponyms and replaced them with the new ones.
