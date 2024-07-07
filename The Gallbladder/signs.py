854f721d8342d8b2e60eddd2669f0ff5369ec4d9
allostericsolutions <ciclosporina2@hotmail.com>
2024-07-06 22:52:49 -0400

    Update signs.py

--- The Gallbladder/signs.py
+++ The Gallbladder/signs.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
 # Initialize the variable in session_state
-if 'mostrar_eponimos' not in st.session_state:
-    st.session_state.mostrar_eponimos = False
+if 'mostrar_signos' not in st.session_state:  # Changed to 'mostrar_signos'
+    st.session_state.mostrar_signos = False
 
-# Function to style the eponym titles
+# Function to style the sign titles
 def style_title(title):
     return f"<span style='color:darkorange; font-weight:bold;'>{title}</span>"
 
 # Gallbladder Signs
-gallbladder_signs = [  # Removed the space and "The" 
+gallbladder_signs = [ 
     ("Ball-on-the-wall sign", "Appearance of a gallbladder polyp."),
     ("Cinnamon bun sign", "Short axis appearance of intussusception."),
     ("Whirlpool sign", "Cystic duct appearance with color Doppler associated with gallbladder torsion."),
@@ -20,6 +20,7 @@ gallbladder_signs = [  # Removed the space and "The"
 ]
 
 # Expandable and collapsible section for Gallbladder Signs
-with st.expander("Gallbladder Ultrasound Signs:", expanded=st.session_state.mostrar_eponimos):
+with st.expander("Gallbladder Ultrasound Signs:", expanded=st.session_state.mostrar_signos):  # Changed to 'mostrar_signos'
     for sign, description in gallbladder_signs:
         st.markdown(style_title(sign) + f": {description}", unsafe_allow_html=True)
+
